cd7eb1fc7df440ca967b62e2f460e6ace6fcf72e
Hannah Zong <yanhuazong@gmail.com>
2025-09-05 11:08:16 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,30 +3,38 @@ import Card1 from './components/Card1'
 import Card2 from './components/Card2'
 import Navbar from './components/Navbar'
 import About from './components/About'
+import Card from './components/Card'
+import Wrapper from './components/Wrapper'
+import img1 from "./assets/headshot-man.png"
+import img2 from "./assets/headshot-woman.png"
 
 function App() {
-
+  const profiles = [
+    {name: "John Doe", title: "Software Engineer", email: "john@example.com", img: img1},
+    {name: "Jane Smith", title: "Product Manager", email: "jane@example.com", img: img2}
+  ]
   return (
     <>
       <header>
         <Navbar />
       </header>
       <main>
+        <Wrapper>
+          <h1>Profile App</h1>
+        </Wrapper>
         <div className="section">
-          <div className="container">
-             <h1>Profile App</h1>
-          </div>
-        </div>
-        <div className="section" id="about">
           <div className="container">
              <About />
           </div>
         </div>
-        <div className="section" id="profiles">
+        <div className="section">
           <div className="container">
              <div className="flex-container">
-                <Card1 />
-                <Card2 />
+              {
+                profiles.map((profile) => (
+                  <Card key={profile.email} name={profile.name} title={profile.title} email={profile.email} img={profile.img} />
+                ))
+              }   
               </div>
           </div>
         </div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const Card = ({name, title, email, img}) => {
+   
+  return (
+    <div className="profile-card">``
+      <div className="profile-card__image">
+        <img src={img} alt={name} />
+      </div>
+      <div className="profile-card__content">
+        <p>{name}</p>
+        <p>{title}</p>
+        <p>
+          <a href={`mailto:${email}`}>{email}</a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes ={
+    name: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired
+}
+export default Card;
