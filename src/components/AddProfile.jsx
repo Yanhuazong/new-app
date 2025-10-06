bcb6205752e3557a7f131b6315e4cc26509f45a8
Hannah Zong <yanhuazong@gmail.com>
2025-10-06 15:32:22 -0400

    Advanced hooks

--- src/components/AddProfile.jsx
+++ src/components/AddProfile.jsx
@@ -1,50 +1,65 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useReducer, useRef, useEffect } from "react";
 import "../styles/addProfile.css";
 import { useNavigate } from "react-router-dom";
 import ProfilesContext from "../contexts/ProfilesContext"
+import { initialState, formReducer } from "../reducers/formReducer";
+
 
 const stripTags = (s) => String(s ?? "").replace(/<\/?[^>]+>/g, "");
 const trimCollapse = (s) =>
   String(s ?? "")
     .trim()
     .replace(/\s+/g, " ");
-const initialValues = {
-  name: "",
-  title: "",
-  email: "",
-  bio: "",
-  img: null,
-};
+// const initialValues = {
+//   name: "",
+//   title: "",
+//   email: "",
+//   bio: "",
+//   img: null,
+// };
 const AddProfile = () => {
-  const [values, setValues] = useState(initialValues);
+  // const [values, setValues] = useState(initialValues);
+  // const { name, title, email, bio, img } = values;
+  // const [errors, setErrors] = useState("");
+  // const [isSubmitting, setIsSubmitting] = useState(false);
+  // const [success, setSuccess] = useState("");
+  const [state, dispatch] =useReducer(formReducer, initialState)
+  const {values, errors, isSubmitting, success} = state
   const { name, title, email, bio, img } = values;
-  const [errors, setErrors] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [success, setSuccess] = useState("");
   const navigate = useNavigate()
 
+  const nameRef = useRef(null)
+  useLayoutEffect(()=>{
+    console.log(nameRef)
+    nameRef.current.focus();
+  }, [])
   const { addProfiles } = useContext(ProfilesContext)
 
   const onChange = (event) => {
     if (event.target.name === "img") {
       const file = event.target.files[0];
-      if (file && file.size < 1024 * 1024) {
-         setErrors("");
-        setValues((prev) => ({ ...prev, img: file }));
-      } else {
-        setErrors("File needs to be less than 1MB.");
-        setValues((prev) => ({ ...prev, img: null }));
-      }
+      // if (file && file.size < 1024 * 1024) {
+      //    setErrors("");
+      //   setValues((prev) => ({ ...prev, img: file }));
+      // } else {
+      //   setErrors("File needs to be less than 1MB.");
+      //   setValues((prev) => ({ ...prev, img: null }));
+      // }
+      const isFileOK = file && file.size < 1024 * 1024
+      dispatch({type: "SET_IMG", payload:isFileOK ? file : null})
     } else {
-      setValues((prev) => ({
-        ...prev,
-        [event.target.name]: event.target.value,
-      }));
+      // setValues((prev) => ({
+      //   ...prev,
+      //   [event.target.name]: event.target.value,
+      // }));
+      const {name, value} = event.target;
+      dispatch({type:"SET_VALUES", payload: {name, value}})
     }
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    setIsSubmitting(true);
+    // setIsSubmitting(true);
+    dispatch({type: "START_SUBMITTING"})
     try {
       const cleanedValues = {
         name: stripTags(trimCollapse(name)),
@@ -54,17 +69,21 @@ const AddProfile = () => {
         img: URL.createObjectURL(img),
       };
       addProfiles(cleanedValues);
-      setSuccess("Form has been submitted successfully.");
-      setValues(initialValues);
+      // setSuccess("Form has been submitted successfully.");
+      // setValues(initialValues);
+      dispatch({type: "SUBMIT_SUCCESS" })
       setTimeout(() => {
-        setSuccess("");
+        // setSuccess("");
+        dispatch({type: "CLEAR_SUCCESS"})
       }, 1000);
       event.currentTarget.reset();
       navigate("/")
     } catch (error) {
-      setErrors("Songthing is wrong.")
+      // setErrors("Something is wrong.")
+      dispatch({type: "HAS_ERROR"})
     } finally {
-      setIsSubmitting(false);
+      // setIsSubmitting(false);
+      dispatch({type: "FINISH_SUBMIT"})
     }
   };
   return (
@@ -72,6 +91,7 @@ const AddProfile = () => {
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input
+          ref={nameRef}
           type="text"
           name="name"
           id="name"
